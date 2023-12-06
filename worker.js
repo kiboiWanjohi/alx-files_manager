347cfc84d3869b02c9648b1e94757532e53d7b0d
kiboiWanjohi <sammy313.sw@gmail.com>
2023-12-06 11:57:09 +0300

    :bug: Update Controller Class Definitions and Tasks

--- worker.js
+++ worker.js
@@ -1,81 +1,59 @@
-/* eslint-disable import/no-named-as-default */
-import { writeFile } from 'fs';
-import { promisify } from 'util';
-import Queue from 'bull/lib/queue';
-import imgThumbnail from 'image-thumbnail';
-import mongoDBCore from 'mongodb/lib/core';
-import dbClient from './utils/db';
-import Mailer from './utils/mailer';
+import Queue from 'bull';
+import { promises } from 'fs';
+import generateThumbnail from 'image-thumbnail';
+import dBClient from './utils/db';
+import { FilesCollection } from './utils/file';
 
-const writeFileAsync = promisify(writeFile);
-const fileQueue = new Queue('thumbnail generation');
-const userQueue = new Queue('email sending');
+const { writeFile } = promises;
 
-/**
- * Generates the thumbnail of an image with a given width size.
- * @param {String} filePath The location of the original file.
- * @param {number} size The width of the thumbnail.
- * @returns {Promise<void>}
- */
-const generateThumbnail = async (filePath, size) => {
-  const buffer = await imgThumbnail(filePath, { width: size });
-  console.log(`Generating file: ${filePath}, size: ${size}`);
-  return writeFileAsync(`${filePath}_${size}`, buffer);
-};
+const THUMBNAIL_SIZES = [500, 250, 100];
+
+const fileQueue = new Queue('image-thumbnail-worker', {
+  redis: {
+    host: 'localhost',
+    port: 6379,
+  },
+});
+
+async function createAndSaveThumbnail(path, width) {
+  const thumbnail = await generateThumbnail(
+    path, { width, responseType: 'base64' },
+  );
+  const filePath = `${path}_${width}`;
+  await writeFile(filePath, Buffer.from(thumbnail, 'base64'));
+}
 
 fileQueue.process(async (job, done) => {
-  const fileId = job.data.fileId || null;
-  const userId = job.data.userId || null;
+  const { userId, fileId } = job.data;
+  if (!fileId) { done(new Error('Missing fileId')); }
+  if (!userId) { done(new Error('Missing userId')); }
 
-  if (!fileId) {
-    throw new Error('Missing fileId');
-  }
-  if (!userId) {
-    throw new Error('Missing userId');
-  }
-  console.log('Processing', job.data.name || '');
-  const file = await (await dbClient.filesCollection())
-    .findOne({
-      _id: new mongoDBCore.BSON.ObjectId(fileId),
-      userId: new mongoDBCore.BSON.ObjectId(userId),
-    });
-  if (!file) {
-    throw new Error('File not found');
-  }
-  const sizes = [500, 250, 100];
-  Promise.all(sizes.map((size) => generateThumbnail(file.localPath, size)))
-    .then(() => {
-      done();
-    });
+  const filesCollection = new FilesCollection();
+  const file = await filesCollection.findUserFileById(userId, fileId, false);
+  if (!file) { done(new Error('File not found')); }
+
+  THUMBNAIL_SIZES.forEach(async (size) => {
+    await createAndSaveThumbnail(file.localPath, size);
+  });
+  done();
+});
+
+export const userQueue = new Queue('user-welcome-worker', {
+  redis: {
+    host: 'localhost',
+    port: 6379,
+  },
 });
 
 userQueue.process(async (job, done) => {
-  const userId = job.data.userId || null;
+  const { userId } = job.data;
+  if (!userId) { done(new Error('Missing userId')); }
 
-  if (!userId) {
-    throw new Error('Missing userId');
-  }
-  const user = await (await dbClient.usersCollection())
-    .findOne({ _id: new mongoDBCore.BSON.ObjectId(userId) });
-  if (!user) {
-    throw new Error('User not found');
-  }
-  console.log(`Welcome ${user.email}!`);
-  try {
-    const mailSubject = 'Welcome to ALX-Files_Manager by B3zaleel';
-    const mailContent = [
-      '<div>',
-      '<h3>Hello {{user.name}},</h3>',
-      'Welcome to <a href="https://github.com/B3zaleel/alx-files_manager">',
-      'ALX-Files_Manager</a>, ',
-      'a simple file management API built with Node.js by ',
-      '<a href="https://github.com/B3zaleel">Bezaleel Olakunori</a>. ',
-      'We hope it meets your needs.',
-      '</div>',
-    ].join('');
-    Mailer.sendMail(Mailer.buildMessage(user.email, mailSubject, mailContent));
-    done();
-  } catch (err) {
-    done(err);
-  }
+  const user = await dBClient.findUserById(userId);
+  if (!user) { done(new Error('User not found')); }
+
+  console.log(`Welcome ${user.email}`);
+  done();
 });
+
+export default fileQueue;
